9737a181f976c233a954abc0a0397b1370f025c1
Ricardo Sousa <rwmsousa@gmail.com>
2023-11-15 17:18:05 -0300

    feat: cria home page e componente button

---
+++ components/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({type, text, onClick }) => {
+  return (
+    <button
+      type="button"
+      onClick={ onClick }
+      className={ `button ${type}`}
+    >
+      { text }
+    </button>
+  );
+};
+
+export default Button;
