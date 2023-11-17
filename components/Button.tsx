403be152b60c2a9425feb530f6353515ef85fe90
Ricardo Sousa <rwmsousa@gmail.com>
2023-11-17 17:03:41 -0300

    build: configura eslint prettier husky

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Button = ({ type, text, onClick }) => {
-    return (
+const Button = ({ type, text, onClick }) => (
         <button type="button" onClick={onClick} className={`button ${type}`}>
             {text}
         </button>
     );
-};
 
 export default Button;
