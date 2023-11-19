c9d1e584e33f8988c70446a6b9df3b51c697f2ba
Ricardo Sousa <rwmsousa@gmail.com>
2023-11-19 16:45:31 -0300

    style: correções de css

--- components/Button2.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import './style.module.scss';
-
-export default function Button2() {
-  return (
-    <button className="StyledButton">Sass</button>
-  );
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 // pages/index.tsx
 import React from 'react';
 import Button from '../components/Button';
-import Button2 from '../components/Button2';
 
 const Home: React.FC = () => {
     const handleClick = (msg: string) => {
@@ -11,13 +10,20 @@ const Home: React.FC = () => {
     return (
         <div>
             <h1>Boilerplate SASS 7-1</h1>
-            <p>Esta é a página inicial do seu projeto Next.js.</p>
+            <h2>Texto H2</h2>
+            <h3>Texto H3</h3>
+            <h4>Texto H4</h4>
+            <h5>Texto H5</h5>
+            <h6>Texto H6</h6>
+            <p>Parágrafo</p>
+            <p>
+                <a href="#">Link</a>
+            </p>
             <Button
                 type="primary"
                 text="Botão primário!"
                 onClick={() => handleClick('Botão primário!')}
             />
-            <Button2 />
         </div>
     );
 };
