66dcad99c83132a233e69804893c6d1321e75b40
Ricardo Sousa <rwmsousa@gmail.com>
2023-12-11 15:38:27 -0300

    fix: errors types

--- providers/api.ts
+++ providers/api.ts
@@ -1,4 +1,9 @@
-// // Blog
-// export const posts = () => {
-//   return api.get(`/2021-blogs?_limit=${ limit }`);
-// };
+import articlesExample from '../static/articleExample';
+
+export default async function getArticles() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(articlesExample);
+    }, 1500);
+  });
+}
